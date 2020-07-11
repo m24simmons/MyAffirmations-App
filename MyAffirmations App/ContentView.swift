c516e5de603df46628ce232e993d49294e0684d0
Megan Simmons <m24simmons@gmail.com>
2020-07-10 19:40:45 -0500

    Initial Commit

---
+++ MyAffirmations App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAffirmations App
+//
+//  Created by Student on 7/10/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
